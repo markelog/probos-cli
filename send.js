1eab0aa8ead32cb3f56feb697d6bd511032407f8
Oleg Gaidarenko <markelog@gmail.com>
2020-02-05 11:22:10 +0300

    Remove artefact

--- send.js
+++ send.js
@@ -3,8 +3,6 @@ const fetch = require('isomorphic-unfetch');
 const API = process.env.PROBOS_API || `http://localhost:8080`;
 
 module.exports = data => {
-  console.log(JSON.stringify(data, null, 4));
-
   return fetch(`${API}/reports`, {
     method: 'POST',
     body: JSON.stringify(data)
